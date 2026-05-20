52aca5ee6da37d9c11112442ff8d80d3d822506e
MiggRocha <rochamigg@gmail.com>
2026-05-20 18:03:33 -0300

    0520 - form

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     nome = "CuboML.com"
-    return render_template('src/index.html', site = nome)
+    return render_template('index.html', site=nome)
+
+
+@app.route("/login")
+def login():
+
+
+return render_template('login/login.html')
+
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
+
 
 if __name__ == "__main__":
     main()

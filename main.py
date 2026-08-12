a375c6c9d0cc74128bd0d2771295e5afeabf0a81
Rafael Sgorlon <rafaelnet2009@gmail.com>
2026-08-12 17:27:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,20 @@
 import os
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-from flask import Flask, render_template, request
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
-cred = credentials.Certificate("firebase-admin.json")
-
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-
 @app.route("/")
 def index():
-    nome = "CuboML.com"
-    return render_template('index.html', site=nome)
+    nome = 'icoma.com.br'
+    return render_template('index.html', site = nome)
 
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login")
 def login():
-
-    if request.method == "POST":
-
-        nome = request.form["nome"]
-        senha = request.form["senha"]
-
-        db.collection("usuarios").add({
-            "nome": nome,
-            "senha": senha
-        })
-
-        return "Usuário cadastrado com sucesso!"
-
     return render_template('login/login.html')
-
-
-@app.route("/teste")
-def teste():
-
-    db.collection("teste").add({
-
-        "mensagem": "Funcionou!",
-        "autor": "Lucas"
-
-    })
-
-    return "Dados enviados com sucesso!"
-
-
 
 def main():
     app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
-    
-
 
 if __name__ == "__main__":
     main()
